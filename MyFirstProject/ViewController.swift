3b96590a17564e373221588cc0ca8680e620268c
Ismail <ismailmohammad@Ismails-MBP.attlocal.net>
2021-06-25 08:44:59 -0500

    Commit as change color button
    Commit as change color  button

--- MyFirstProject/ViewController.swift
+++ MyFirstProject/ViewController.swift
@@ -12,8 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+        let button = UIButton()
+        button.frame = CGRect(x: 40, y: 100, width: 100, height: 50)
+        button.backgroundColor = .blue
+        button.setTitle("Change", for: .normal)
+        button.addTarget(self, action: #selector(changeColor), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+        
     }
-
+    @objc func changeColor() {
+        self.view.backgroundColor = .green
+    }
 
 }
 
